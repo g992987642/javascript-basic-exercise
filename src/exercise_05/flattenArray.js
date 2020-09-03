92d72b29f08eea4e12c7d2c6509c415c769db6c4
g992987642 <992987642@qq.com>
2020-09-03 20:06:17 +0800

    guhao feat:finished exercise_05

--- src/exercise_05/flattenArray.js
+++ src/exercise_05/flattenArray.js
@@ -6,5 +6,21 @@ export default function flattenArray(array) {
   // * Please implement this function and pass all the tests in flatten_array_spec.js.
   // * Please do NOT modify the signature of the function.
 
-  throw new Error('Please delete this line and implement the function');
+
+  if (array === undefined || array === null) {
+    throw Error('Flatten undefined or null array');
+  }
+  const result = [];
+
+  for (let i = 0; i < array.length; i += 1) {
+    if (!Array.isArray(array[i])) {
+      result.push(array[i]);
+    } else {
+      const temp = array[i];
+      for (let n = 0; n < temp.length; n += 1) {
+        result.push(temp[n]);
+      }
+    }
+  }
+  return result;
 }
