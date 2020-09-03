b40ab3af3b909f8e0a4a2ded757ddfa9ae83935d
g992987642 <992987642@qq.com>
2020-09-03 20:06:08 +0800

    guhao feat:finished exercise_04

--- src/exercise_04/arrayToList.js
+++ src/exercise_04/arrayToList.js
@@ -6,5 +6,35 @@ export default function arrayToList(array) {
   // * Please implement this function and pass all the tests in array_to_list_spec.js.
   // * Please do NOT modify the signature of the function.
 
-  throw new Error('Please delete this line and implement the function');
+  if (array === undefined || array === null) {
+    throw Error('Creating list from undefined array');
+  }
+
+  if (array.length === 0) {
+    throw Error('Creating list from empty array');
+  }
+  function LinkedList() {
+    this.head = null;
+    this.tail = null;
+
+    function Node(value) {
+      this.value = value;
+      this.next = null;
+    }
+    this.insert = function insert(arrays) {
+      for (let i = 0; i < arrays.length; i += 1) {
+        const curNode = new Node(arrays[i]);
+        if (this.head == null) {
+          this.head = curNode;
+          this.tail = curNode;
+        } else {
+          this.tail.next = curNode;
+          this.tail = curNode;
+        }
+      }
+    };
+  }
+  const resultList = new LinkedList();
+  resultList.insert(array);
+  return resultList.head;
 }
