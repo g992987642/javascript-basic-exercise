0549277dc1224d3d530d7145cc90e842506304b9
g992987642 <992987642@qq.com>
2020-09-03 20:06:34 +0800

    guhao feat:finished exercise_07

--- src/exercise_07/waitForAll.js
+++ src/exercise_07/waitForAll.js
@@ -1,13 +1,29 @@
 export default function waitForAll(...promises) {
-  // This function returns a promise which will be triggered when all the `promises` are completed.
-  //
-  // If all the `promises` are resolved, then the returned promise will be resolved. Otherwise,
-  // if one of the `promises` is rejected, then the returned promise will be rejected.
-  //
-  // Your target:
-  //
-  // * Please implement this function and pass all the tests in wait_for_all_spec.js.
-  // * Please do NOT modify the signature of the function.
+  promises.forEach((promise) => {
+    if (!(promise instanceof Promise)) {
+      throw Error('Not all elements are promises.');
+    }
+  });
 
-  throw new Error('Please delete this line and implement the function');
+  return new Promise((resolve, reject) => {
+    let counter = 0;
+    for (let i = 0; i < promises.length; i += 1) {
+      // eslint-disable-next-line no-loop-func
+      promises[i].then(() => {
+        counter += 1;
+        if (counter === promises.length) {
+          resolve();
+        }
+      },
+      // eslint-disable-next-line no-loop-func
+      () => {
+        'It should not be a success promise';
+      // eslint-disable-next-line no-loop-func
+      }).finally(() => {
+        if (i === promises.length - 1 && counter !== promises.length) {
+          reject();
+        }
+      });
+    }
+  });
 }
